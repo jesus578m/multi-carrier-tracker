39576bc1302d8b1acd3d5a4cf01855b178646458
jesus578m <122491133+jesus578m@users.noreply.github.com>
2025-08-28 11:44:15 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import('./server.js').then(({ default: app }) => {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+});
